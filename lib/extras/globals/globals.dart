69df63d985788e325c3130775b4f4f1a77483fe3
YefrixD <gerson.vilca@tecsup.edu.pe>
2019-08-30 23:32:52 -0500

    Nuevo

--- lib/extras/globals/globals.dart
+++ lib/extras/globals/globals.dart
@@ -2,36 +2,32 @@
 library my_prj.globals;
 
 
-String urlGlobal =  'https://illapa-bb.appspot.com';
-String urlGestion =  'https://illapa-bb.appspot.com/api/gestion';
-String urlGestionSectoristas =  'https://illapa-bb.appspot.com/api/sectorista/gestion';
-String urlGestionFree =  'https://illapa-bb.appspot.com/api/gestionFree';
+String urlGlobal =  'http://192.168.1.8';
+String urlGestion =  'http://192.168.1.8/api/gestion';
+String urlGestionSectoristas =  'http://192.168.1.8/api/sectorista/gestion';
+String urlGestionFree =  'http://192.168.1.8/api/gestionFree';
 
-String urlVencimiento =  'https://illapa-bb.appspot.com/api/vencimiento';
+String urlVencimiento =  'http://192.168.1.8/api/vencimiento';
 
-String urlEstadistica =  'https://illapa-bb.appspot.com/api/estadistica';
-String urlEstadisticasGestor =  'https://illapa-bb.appspot.com/api/estadistica/gestor';
-String urlEstadisticasSectoristas =  'https://illapa-bb.appspot.com/api/estadistica/sectorista';
-String urlEstFree =  'https://illapa-bb.appspot.com/api/estadisticasFree';
+String urlEstadistica =  'http://192.168.1.8/api/estadistica';
+String urlEstadisticasGestor =  'http://192.168.1.8/api/estadistica/gestor';
+String urlEstadisticasSectoristas =  'http://192.168.1.8/api/estadistica/sectorista';
+String urlEstFree =  'http://192.168.1.8/api/estadisticasFree';
 
 
-String urlDato =  'https://illapa-bb.appspot.com/api/dato';
-String urlDatFree =  'https://illapa-bb.appspot.com/api/datoFree';
+String urlDato =  'http://192.168.1.8/api/dato';
+String urlDatFree =  'http://192.168.1.8/api/datoFree';
 
-String urlUsuario =  'https://illapa-bb.appspot.com/api/usuario';
+String urlUsuario =  'http://192.168.1.8/api/usuario';
 
-String urlTramo =  'https://illapa-bb.appspot.com/api/tramo';
-String urlTramoFree =  'https://illapa-bb.appspot.com/api/tramoFree';
+String urlTramo =  'http://192.168.1.8/api/tramo';
+String urlTramoFree =  'http://192.168.1.8/api/tramoFree';
       
-
-// String urlSubirImagen = 'https://illapa-bb.appspot.com/api/datoFree';
-// String urlImagenes = 'https://illapa-bb.appspot.com/';
+// String urlSubirImagen = 'http://35.222.139.100/api/datoFree';
+// String urlImagenes = 'http://35.222.139.100/';
 
 String urlSubirImagen = 'http://35.222.139.100/api/datoFree';
 String urlImagenes = 'http://35.222.139.100/';
-
-
-
 
 
 String imagenAdmin = 'https://cdn.pixabay.com/photo/2015/03/04/22/35/head-659651_960_720.png';
